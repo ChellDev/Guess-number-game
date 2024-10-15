c861f57c59feb0ae816e143e192297498d7932f6
ChellDev <michell.camargo@yahoo.com.br>
2024-10-15 00:10:31 -0300

    Aprendendo a utilizar loop infinito

---
+++ rock_paper_scissors.py
@@ -0,0 +1,12 @@
+""" * Utilizando loop infinito
+
+"""
+
+import random
+
+user_pointes = 0
+computer_points = 0
+
+while True:
+    user_choice = input("Escolha R(Pedra)/T(Tesoura)/P(Papel) ou  Q para sair.").lower()
+    
